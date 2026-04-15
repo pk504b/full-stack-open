40263e1622addf588a5072e3fb343659202856e1
Prince <pk.504b@gmail.com>
2026-04-15 20:36:53 +0200

    3.9: add cors to phonebook backend

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,85 @@
+const express = require("express")
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let data =[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${data.length} persons</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(data)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = data.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+
+  if (!person.name || !person.number) {
+    res.status(400).json({ error: "Missing name or number" })
+    return
+  }
+
+  if (data.find(p => p.name === person.name)) {
+    res.status(400).json({ error: "Name already exists" })
+    return
+  }
+
+  person.id = Math.floor(Math.random() * 10000);
+  data.push(person)
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  data = data.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
