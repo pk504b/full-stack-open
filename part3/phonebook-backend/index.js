1260e95e41136114c169fbbd63bc6271893c9260
Prince <pk.504b@gmail.com>
2026-04-21 16:52:43 +0200

    3.15: Phonebook database, step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -12,29 +12,6 @@ morgan.token('body', (req) => {
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let data =[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (req, res) => {
   res.send(`
     <p>Phonebook has info for ${data.length} persons</p>
@@ -76,8 +53,11 @@ app.post('/api/persons', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  data = data.filter(person => person.id !== id)
-  res.status(204).end()
+  Person.findByIdAndDelete(id).then(person => {
+    res.status(204).end()
+  }).catch(err => {
+    res.status(404).end()
+  })
 })
 
 const PORT = process.env.PORT
