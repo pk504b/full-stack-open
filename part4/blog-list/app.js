f3c61c9b0ca2936ff9d10054d10f059049d71d47
Prince <pk.504b@gmail.com>
2026-04-21 23:48:29 +0200

    4.2: add logger & middleware, refactor complete

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -2,14 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 const app = express()
+app.use(middleware.requestLogger)
 
 mongoose.connect(config.MONGO_URI, { family: 4 })
-.then(() => console.log('Connected to database'))
-.catch(err => console.error(err))
+.then(() => logger.info('Connected to database'))
+.catch(err => logger.error(err))
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
