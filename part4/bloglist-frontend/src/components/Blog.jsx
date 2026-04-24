216d6c54697f785c3bfe34c0e3f1875d3da26e19
Prince <pk.504b@gmail.com>
2026-04-24 19:22:38 +0200

    5.7: show/hide blog details

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <div style={{
+      border: "1px solid black",
+      padding: "4px",
+      margin: "4px 0"
+    }}>
+      <div>
+        {blog.title} <button onClick={() => setExpanded(!expanded)}>
+          {expanded ? "hide" : "show"}
+        </button>
+      </div>
+
+      {expanded && (
+        <div>
+          {blog.url} <br />
+          {blog.likes} <button>like</button> <br />
+          {blog.author}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
