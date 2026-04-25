5cb3d619fa6d3f7294b19853f68e02dc4ec67c7c
Prince <pk.504b@gmail.com>
2026-04-25 21:14:13 +0200

    5.15: test like button

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -37,5 +37,18 @@ describe("Blog", () => {
     expect(screen.getByText("0 likes", { exact: false })).toBeDefined()
     expect(screen.getByText("test user", { exact: false })).toBeDefined()
   })
+  test("clicking on like button should call incrementLike", async () => {
+    const mockHandler = vi.fn()
+    render(<Blog blog={testBlog} incrementLike={mockHandler} />)
+    // Expand blog
+    const expandButton = screen.getByText("show")
+    await userEvent.click(expandButton)
+    // Test liking
+    const user = userEvent.setup()
+    const likeButton = screen.getByText("like")
+    await user.click(likeButton)
+    await user.click(likeButton)
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
 })
 
