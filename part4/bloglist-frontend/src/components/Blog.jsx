7fcd476c9a7eef90ba8f73ae70782b1a1416298a
Prince <pk.504b@gmail.com>
2026-04-27 19:33:22 +0200

    5.31 style Blog as card

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,3 +1,4 @@
+import { Button, Card, CardContent, CardHeader, Typography } from "@mui/material"
 import { useParams } from "react-router-dom"
 
 const Blog = ({ loggedinUser, blogs, increaseLike, removeBlog }) => {
@@ -7,20 +8,21 @@ const Blog = ({ loggedinUser, blogs, increaseLike, removeBlog }) => {
   if (!blog) return <div>blog not found</div>
 
   return (
-    <div className="blog">
-      <h3>
-        {blog.title} by {blog.author}
-      </h3>
-
-      <div>
-        {blog.url} <br />
-        {blog.likes} likes {loggedinUser && <button onClick={() => increaseLike(blog.id, blog)}>like</button>}
+    <Card className="blog" style={{ marginTop: "5rem" }}>
+      <CardContent>
+        <Typography variant="h4">{blog.title}</Typography>
+        <Typography variant="h6">by {blog.author}</Typography>
+        <Typography variant="body1"><a href={blog.url}>{blog.url}</a></Typography>
         <br />
-        {blog.user.name}
-        <br />
-        {loggedinUser && blog.user.username === loggedinUser.username && <button onClick={() => removeBlog(blog.id)}>remove</button>}
-      </div>
-    </div>
+        <Typography variant="body1">
+          {blog.likes} likes
+          {" "}
+          {loggedinUser && <Button variant="contained" color="primary" onClick={() => increaseLike(blog.id, blog)}>like</Button>}
+          { " " }
+          {loggedinUser && blog.user.username === loggedinUser.username && <Button variant="outlined" color="error" onClick={() => removeBlog(blog.id)}>remove</Button>}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
