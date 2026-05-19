20c7416189af70372b8694bd2f97104bdeffacf4
Prince <pk.504b@gmail.com>
2026-05-19 11:19:48 +0200

    10.7: scrollview

--- part10-native/rate-repository-app/src/components/AppBar.jsx
+++ part10-native/rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import { Link } from 'react-router-native';
 
@@ -8,26 +8,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#24292e',
     color: '#fff',
     padding: 20,
+    flexDirection: 'row',
+    gap: 10,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#fff',
   },
+  button: {
+    paddingHorizontal: 14,
+    paddingVertical: 5,
+    borderRadius: 8,
+  },
 });
 
 const AppBar = () => {
   return <View style={styles.container}>
-    <Pressable>
-      <Link to="/signin">
-        <Text style={styles.title}>Sign In</Text>
-      </Link>
-    </Pressable>
-    <Pressable>
-      <Link to="/repositories">
-        <Text style={styles.title}>Repositories</Text>
-      </Link>
-    </Pressable>
+    <ScrollView horizontal>
+      <Pressable>
+        <Link to="/signin" style={styles.button}>
+          <Text style={styles.title}>Sign In</Text>
+        </Link>
+      </Pressable>
+      <Pressable >
+        <Link to="/repositories" style={styles.button}>
+          <Text style={styles.title}>Repositories</Text>
+        </Link>
+      </Pressable>
+    </ScrollView>
   </View>;
 };
 
