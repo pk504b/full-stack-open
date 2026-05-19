0cade9af35a3297c1120ec23d4b79fce53326c50
Prince <pk.504b@gmail.com>
2026-05-19 19:01:39 +0200

    10.16: signout

--- part10-native/rate-repository-app/src/components/AppBar.jsx
+++ part10-native/rate-repository-app/src/components/AppBar.jsx
@@ -1,6 +1,9 @@
 import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
-import { Link } from 'react-router-native';
+import { Link, useNavigate } from 'react-router-native';
+import { useApolloClient, useQuery } from '@apollo/client/react';
+import { GET_ME } from '../graphql/queries';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const styles = StyleSheet.create({
   container: {
@@ -24,13 +27,32 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data, loading } = useQuery(GET_ME);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+  const handleSignout = async () => {
+    await authStorage.removeAccessToken();
+    apolloClient.resetStore();
+    navigate('/signin');
+  };
+
+  if (loading) return <Text>Loading...</Text>;
+
   return <View style={styles.container}>
     <ScrollView horizontal>
-      <Pressable>
-        <Link to="/signin" style={styles.button}>
-          <Text style={styles.title}>Sign In</Text>
-        </Link>
-      </Pressable>
+      {data && data.me ? (
+        <Pressable style={styles.button} onPress={handleSignout}>
+          <Text style={styles.title}>Sign out</Text>
+        </Pressable>
+      ) : (
+        <Pressable>
+          <Link to="/signin" style={styles.button}>
+            <Text style={styles.title}>Sign In</Text>
+          </Link>
+        </Pressable>
+      )}
       <Pressable >
         <Link to="/repositories" style={styles.button}>
           <Text style={styles.title}>Repositories</Text>
--- part10-native/rate-repository-app/src/graphql/queries.js
+++ part10-native/rate-repository-app/src/graphql/queries.js
@@ -1,5 +1,16 @@
 import { gql } from '@apollo/client';
 
+export const GET_ME = gql`
+  query GetMe {
+    me {
+      id
+      username
+      createdAt
+      reviewCount
+    }
+  }
+`;
+
 export const GET_REPOSITORIES = gql`
   query GetRepositories {
     repositories {
@@ -17,3 +28,4 @@ export const GET_REPOSITORIES = gql`
     }
   }
 `;
+
