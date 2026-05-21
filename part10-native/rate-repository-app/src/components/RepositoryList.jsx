79d6fc731a57d8a745df1165b7539c3891fd955f
Prince <pk.504b@gmail.com>
2026-05-21 22:13:52 +0200

    10.24: filter

--- part10-native/rate-repository-app/src/components/RepositoryList.jsx
+++ part10-native/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,59 +1,64 @@
-import { FlatList, Pressable, Text } from 'react-native';
+import { FlatList, Pressable, Text, TextInput } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import { useQuery } from '@apollo/client/react';
 import { GET_REPOSITORIES } from '../graphql/queries';
 import { useNavigate } from 'react-router-native';
 import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
+import useDebounce from '../hooks/useDebounce';
 
 export const RepositoryListContainer = ({ data, onRepositoryPress }) => {
-  
-
   const repositoryNodes = data
     ? data.repositories.edges.map((edge) => edge.node)
     : [];
 
   return (
-    <>
-      
-
-      <FlatList
-        data={repositoryNodes}
-        renderItem={({ item }) => (
-          <Pressable onPress={() => onRepositoryPress(item.id)}>
-            <RepositoryItem repository={item} />
-          </Pressable>
-        )}
-      />
-    </>
+    <FlatList
+      data={repositoryNodes}
+      renderItem={({ item }) => (
+        <Pressable onPress={() => onRepositoryPress(item.id)}>
+          <RepositoryItem repository={item} />
+        </Pressable>
+      )}
+    />
   )
 }
 
 const RepositoryList = () => {
   const [sortBy, setSortBy] = useState('latest');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const debouncedSearchKeyword = useDebounce(searchKeyword, 500);
 
   const { data, loading } = useQuery(GET_REPOSITORIES, {
     variables: {
       orderBy: sortBy === 'latest' ? 'CREATED_AT' : 'RATING_AVERAGE',
       orderDirection: sortBy === 'latest' ? 'DESC' : sortBy === 'highestRated' ? 'DESC' : 'ASC',
+      searchKeyword: debouncedSearchKeyword,
     },
     fetchPolicy: 'cache-and-network',
   });
   const navigate = useNavigate();
 
-  if (loading) return <Text>Loading...</Text>;
+  if (loading && !data) return <Text>Loading...</Text>;
 
   return (
     <>
-      <Picker
-        selectedValue={sortBy}
-        onValueChange={(itemValue, itemIndex) =>
-          setSortBy(itemValue)
-        }>
-        <Picker.Item label="Latest" value="latest" />
-        <Picker.Item label="Highest rated" value="highestRated" />
-        <Picker.Item label="Lowest rated" value="lowestRated" />
-      </Picker>
+      <TextInput
+        placeholder="Search repositories"
+        style={{ padding: 10, borderRadius: 8, margin: 10, borderWidth: 1, borderColor: '#000' }}
+        value={searchKeyword}
+        onChangeText={(text) => setSearchKeyword(text)}
+      />
+        <Picker
+          selectedValue={sortBy}
+          style={{ marginHorizontal: 10 }}
+          onValueChange={(itemValue, itemIndex) =>
+            setSortBy(itemValue)
+          }>
+          <Picker.Item label="Latest" value="latest" />
+          <Picker.Item label="Highest rated" value="highestRated" />
+          <Picker.Item label="Lowest rated" value="lowestRated" />
+        </Picker>
 
       <RepositoryListContainer
         data={data}
--- part10-native/rate-repository-app/src/graphql/queries.js
+++ part10-native/rate-repository-app/src/graphql/queries.js
@@ -12,8 +12,8 @@ export const GET_ME = gql`
 `;
 
 export const GET_REPOSITORIES = gql`
-  query GetRepositories($orderBy: AllRepositoriesOrderBy, $orderDirection: OrderDirection) {
-    repositories(orderBy: $orderBy, orderDirection: $orderDirection) {
+  query GetRepositories($orderBy: AllRepositoriesOrderBy, $orderDirection: OrderDirection, $searchKeyword: String) {
+    repositories(orderBy: $orderBy, orderDirection: $orderDirection, searchKeyword: $searchKeyword) {
       edges {
         node {
           id
